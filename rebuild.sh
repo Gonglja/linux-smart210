0b9f1e92e389171c5a8d9bffde83128d44a73431
Gonglja <glj0@outlook.com>
2022-05-17 13:43:44 +0800

    修改编译脚本

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+## ubuntu 22.04 lts
+## 0.1 Install corss-compile-toochain
+# sudo apt install gcc-arm-none-eabi
+
+## 0.2 Dep
+# sudo apt install flex bison
+# sudo apt-get install libssl-dev
+# sudo apt install u-boot-tools 
+
+## 0.3 issue
+# fdt error
+# sudo apt-get purge -y --auto-remove libfdt-dev
 export ARCH=arm
 export CROSS_COMPILE=arm-none-eabi-
 
