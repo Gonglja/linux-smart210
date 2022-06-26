b1828d04db51db3178d228e019e4ce3bff7d39c7
Gonglja <glj0@outlook.com>
2022-06-26 21:54:23 +0800

    FEAT: 修改编译脚本

--- rebuild.sh
+++ rebuild.sh
@@ -15,10 +15,28 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-none-eabi-
 
-make clean && make distclean 
-make s5pv210_fin_defconfig 
+# 0.4 配置编译链环境
+export ARCH=arm
+export CROSS_COMPILE=arm-none-eabi-
+
+# 0.5  清除以前编译
+make clean && make distclean
+
+
+# 1.0 配置s5pv210默认配置
+make s5pv210_fin_defconfig
+
+# 1.1 编译 uImage
 make LOADADDR=0x20004000 uImage -j$(nproc)
+
+# 1.2 编译 modules 并安装至./output下
+make modules -j$(nproc)
+make INSTALL_MOD_PATH=./output modules_install
+
+# 1.3 编译 dtbs 并安装至./output下
 make dtbs -j$(nproc)
+make INSTALL_DTBS_PATH=./output dtbs_install 
+
 
 echo "copy to ../rootfs/"
 sudo cp -vrf arch/arm/boot/uImage ../rootfs/
